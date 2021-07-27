81956bf15672740168895d2c895cb6a4025110d8
jerome <jeromebrown97@gmail.com>
2021-07-27 10:49:45 -0500

    inital commit for TDD Demo

---
+++ TDDDemo/FizzBuzz.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDDemo
+{
+    class FizzBuzz
+    {
+       public string GetFizzBuzz(int num)
+        {
+            if(num % 5 ==0 && num % 3 == 0)
+            {
+                return "fizzbuzz";
+            }
+            else if(num % 5 == 0)
+            {
+                return "fizz";
+            }
+            else if(num %3==0)
+            {
+                return "buzz";
+            }
+            else
+            {
+                return "";
+            }
+            
+        }
+
+    }
+}
---
+++ TDDDemo/FizzBuzzTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit; 
+
+namespace TDDDemo
+{
+    public class FizzBuzzTest
+    {
+      [Fact]
+      public void TestFizz()
+        {
+            //Assemble 
+            FizzBuzz fb = new FizzBuzz();
+            string expected = "fizz";
+
+            //Act
+            string actual = fb.GetFizzBuzz(5);
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Theory]
+        [InlineData(3, "buzz")]
+        [InlineData(1, "")]
+        [InlineData(15, "fizzbuzz")]
+        public void TestFizzBuzz(int input, string expected)
+        {
+            FizzBuzz fb = new FizzBuzz();
+
+            string actual = fb.GetFizzBuzz(input);
+
+            Assert.Equal(expected, actual);
+        }
+
+    }
+}
